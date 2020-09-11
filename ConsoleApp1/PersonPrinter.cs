6a9ee85ea0ea1f416e87e3c75fbc12c30595671f
Rasmus Hoeyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2020-09-11 10:03:12 +0200

    did stuff

---
+++ ConsoleApp1/PersonPrinter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class PersonPrinter
+    {
+        public void PrintFamilyTree(Person person)
+        {
+            PrintPerson(person);
+            if (person.Far != null)
+            {
+                PrintFamilyTree(person.Far);
+            }
+        }
+
+        private void PrintPerson(Person person)
+        {
+            // Console.WriteLine($"{person.Fornavn}");
+            Console.WriteLine(person);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Laver ny person
+            Person person1 = new Person();
+            //Sætter fornavn
+            person1.Fornavn = "Joakim";
+
+            //person 3 med fornavn Peter
+            Person person3 = new Person
+            {
+                Fornavn = "Peter"
+            };
+
+            //Person2 med far person3
+            Person person2 = new Person(person3);
+            person2.Fornavn = "Sylvester";
+
+            //consolewrite med person1 fornavn
+            Console.WriteLine(person1.Fornavn);
+
+            //writeline med tostring metode
+            Console.WriteLine(person1);
+
+            person1.Far = person2;
+            person2.Far = person3;
+            Console.WriteLine("{0}'s far: {1} og bedstefar: {2}", 
+                person1.Fornavn, person1.Far.Fornavn, person1.Far.Far.Fornavn);
+
+            //Ny printer
+            PersonPrinter printer = new PersonPrinter();
+            //Kald af PrintFamilyTree
+            printer.PrintFamilyTree(person1);
+
+            
+        }
+    }
+}
